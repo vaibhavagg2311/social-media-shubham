9c1479a7b41cef4aa3bc3f59077c0b8bac6d08b0
vaibhavagg2311 <vaibhavagg2311@gmail.com>
2020-11-10 22:48:25 +0530

    shubham

---
+++ elmer-master/messenger/api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from .views import (
+    ContactsListAPIView, MessageCreateAPIView, MessageListAPIView,
+)
+
+urlpatterns = [
+    url(r'^contacts/$', ContactsListAPIView.as_view(), name='list_contacts'),
+    url(r'^chat/$', MessageListAPIView.as_view(), name='list_messages'),
+    url(r'^send/$', MessageCreateAPIView.as_view(), name='create_message'),
+]
